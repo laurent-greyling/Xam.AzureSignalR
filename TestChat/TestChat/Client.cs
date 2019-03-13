712b5723a6517d68c6fad25a8d65504d0620f26b
Laurent Greyling <L.Greyling@nipo.com>
2019-03-13 12:24:57 +0100

    Work with self signed certificate against the hub

--- TestChat/TestChat/Client.cs
+++ TestChat/TestChat/Client.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +19,18 @@ namespace TestChat
         {
             try
             {
-                Connection = new HubConnectionBuilder().WithUrl("https://127.0.0.1:44333/clienthub").Build();
+                Connection = new HubConnectionBuilder().WithUrl("https://localhost:44333/clienthub", options =>
+                {
+                    options.HttpMessageHandlerFactory = (handler) =>
+                    {
+                        if (handler is HttpClientHandler clientHandler)
+                        {
+                            clientHandler.ServerCertificateCustomValidationCallback = ValidateCertificate;
+                        }
+                        return handler;
+                    };
+                }).Build();
+
                 await Connection.StartAsync();
 
                 Connection.On<string, string>("BroadcastMessage", (name, message)=> 
@@ -37,6 +51,13 @@ namespace TestChat
                 throw;
             }
             
+        }
+
+        bool ValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            // TODO: You can do custom validation here, or just return true to always accept the certificate.
+            // DO NOT use custom validation logic in a production application as it is insecure.
+            return true;
         }
 
         public async Task Broadcast(string name, string message)
